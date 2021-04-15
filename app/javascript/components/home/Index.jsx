d26cd0b9f57c599f393d23f4bc76e679d44d523f
Kalu Nwanna <nwannauk@gmail.com>
2021-04-15 06:26:21 +0100

    add more data

--- app/javascript/components/home/Index.jsx
+++ app/javascript/components/home/Index.jsx
@@ -78,7 +78,7 @@ const HomePage = () => {
           <p className="city">{`${weatherData.name}, 
           ${weatherData.sys.country}, 
           ${weatherData.weather[0].description}`}</p>
-          <p>{`Wind ${weatherData.wind.speed}, Coud ${weatherData.clouds.all}`} </p>
+          <p>{`Wind ${weatherData.wind.speed}m/s, Coud ${weatherData.clouds.all}%`} </p>
           <p>{`Geo cords[${weatherData.coord.lon, weatherData.coord.lat}]`}</p>
         </div>
       )}
